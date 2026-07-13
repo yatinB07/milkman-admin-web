d83efae4d9757c06c5f7a80eca69ff74adc25ec8
YatinB07 <ybhanderi@gmail.com>
2026-07-13 15:14:29 +0530

    refactor(forms): use neutral input style hook
    - Rename the shared input CSS hook away from admin-specific wording
    
    - Remove dead textarea styles left after deleting the unused textarea component
    
    - Verified with npm run lint, npm test -- --run, and npm run build

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -4,5 +4,5 @@ import { forwardRef, type InputHTMLAttributes } from 'react'
 type InputProps = InputHTMLAttributes<HTMLInputElement>
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(function Input({ className, ...props }, ref) {
-  return <input className={clsx('admin-input', className)} ref={ref} {...props} />
+  return <input className={clsx('input-field', className)} ref={ref} {...props} />
 })
